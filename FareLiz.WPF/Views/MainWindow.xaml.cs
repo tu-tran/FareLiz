a11e73ed85b74c90a8a63dd11047d3b134f8d140
Tran Tu <tu.tran@trimble.com>
2016-05-11 13:47:42 +0300

    Add WPF project

---
+++ FareLiz.WPF/Views/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Interaction logic for SelectionWindow.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace FareLiz.WPF.Views
+{
+    using System.ComponentModel;
+
+    /// <summary>Interaction logic for SelectionWindow.xaml</summary>
+    public partial class MainWindow
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow" /> class.
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
